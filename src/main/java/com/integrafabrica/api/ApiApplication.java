a545090036a857949e030f3c86645c75d2ceb5cd
adilsoncarden <x.userprivate.2023@gmail.com>
2026-05-08 11:26:02 -0500

    feat: Implement backend API integration and rename core components

--- src/main/java/com/integrafabrica/api/ApiApplication.java
+++ src/main/java/com/integrafabrica/api/ApiApplication.java
@@ -1,4 +1,4 @@
-package com.integrafabrica.backend;
+package com.integrafabrica.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
