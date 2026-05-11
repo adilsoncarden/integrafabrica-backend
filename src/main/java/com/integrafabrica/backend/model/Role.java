a606d302fcd1e1406f3b45cd684a742604f688dc
adilsoncarden <x.userprivate.2023@gmail.com>
2026-05-10 21:07:04 -0500

    feat: add user and role entities for postgresql

---
+++ src/main/java/com/integrafabrica/backend/model/Role.java
@@ -0,0 +1,19 @@
+package com.integrafabrica.backend.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+}
